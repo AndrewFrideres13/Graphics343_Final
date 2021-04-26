fffa35b372d69df2c5505c209f9d944b453e6fe1
Andrew F <andrewfrideres13@gmail.com>
2021-04-26 12:02:04 -0500

    Removing old OF files, cleaning things up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,8 @@
 #include "ofApp.h"
 #include "GLFW/glfw3.h"
-#include "CameraMatrices.h"
-#include "SimpleDrawNode.h"
 #include <chrono>
 using namespace std::chrono;
-
+using namespace glm;
 color ray_color(const ray& r, const hittable& world, int depth) {
 	if (depth <= 0) {
 		return color(0, 0, 0);
@@ -23,29 +21,12 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	using namespace glm;
 
 	ofDisableArbTex();
 
 	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
 	ofEnableDepthTest();
-
-	cubeMesh.load("models/cube.ply");
-
-	cubemap.load("skyboxes/cube2_front.jpg", "skyboxes/cube2_back.jpg", "skyboxes/cube2_right.jpg", "skyboxes/cube2_left.jpg", "skyboxes/cube2_top.jpg", "skyboxes/cube2_bottom.jpg");
-
-	cubemap.getTexture().generateMipmap();
-
-	skyboxShader.load("skybox.vert", "skybox.frag");
-
-	testSphere.load("models/sphere.ply");
-
-	testShader.load("mesh.vert", "mesh.frag");
-
-	sceneGraphRoot.childNodes.emplace_back(new SceneGraphNode());
-
-	sceneGraphRoot.childNodes.back()->childNodes.emplace_back(new SimpleDrawNode(testSphere, testShader));
 
 	const int imgHeight = (int)(imgWidth / aspectRatio); // Setting height based on aspect ratio
 	const int samples_per_pixel = 10;
@@ -96,7 +77,7 @@ void ofApp::setup() {
 	//This may need to move before we save the img...not 100% sure yet.
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
-	std::cout << "Elapsed Time: " << duration.count() << "ms" << std::endl;
+	std::cout << "\nElapsed Time: " << duration.count() << "ms" << std::endl;
 }
 
 //--------------------------------------------------------------
@@ -106,39 +87,6 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	using namespace glm;
-
-	/*
-	float width = static_cast<float>(ofGetViewportWidth());
-	float height = static_cast<float>(ofGetViewportHeight());
-	float aspect = width / height;
-
-	mat4 model;
-
-	CameraMatrices camMatrices{ camera, aspect, 0.01f, 20.0f };
-
-	skyboxShader.begin();
-
-	glDepthFunc(GL_LEQUAL);
-
-	skyboxShader.setUniformMatrix4f("mvp", camMatrices.getProj() * mat4(mat3(camMatrices.getView())));
-	skyboxShader.setUniformTexture("cubemap", cubemap.getTexture(), 0);
-
-	cubeMesh.draw();
-
-	skyboxShader.end();
-	glDepthFunc(GL_LESS);
-
-	sceneGraphRoot.drawSceneGraph(camMatrices);
-	*/
-
-	/*
-	testShader.begin();
-
-	testSphere.draw();
-
-	testShader.end();
-	*/
 }
 
 //--------------------------------------------------------------
@@ -193,9 +141,6 @@ void ofApp::windowResized(int w, int h) {
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg) {
 
-}
-
-void ofApp::drawCube(const CameraMatrices& camMatrices) {
 }
 
 //--------------------------------------------------------------
